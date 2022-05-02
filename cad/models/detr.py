07f9df8af767bb61b6e5b87d688874c20fcc702b
Colin <csamplawski@cs.umass.edu>
2022-05-02 00:16:40 -0400

    detr encoder/decoder now do xavier init

--- cad/models/detr.py
+++ cad/models/detr.py
@@ -1,12 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from mmcv.cnn import build_norm_layer
+from mmcv.cnn import build_norm_layer, xavier_init
 from mmcv.cnn.bricks.registry import DROPOUT_LAYERS, ACTIVATION_LAYERS, NORM_LAYERS, ATTENTION,\
         FEEDFORWARD_NETWORK, POSITIONAL_ENCODING
 from mmcv.runner.base_module import BaseModule#, ModuleList
 from mmcv import build_from_cfg
-# from ..builder import BLOCKS
 
 #@BLOCKS.register_module()
 @FEEDFORWARD_NETWORK.register_module()
@@ -17,6 +16,7 @@ class DETRDecoder(BaseModule):
             cross_attn_cfg=None,
             ffn_cfg=None,
             shared_norm_cfg=None,
+            return_all_layers=False,
             init_cfg=None
         ):
         super().__init__(init_cfg)
@@ -25,20 +25,36 @@ class DETRDecoder(BaseModule):
         self.cross_attns = [build_from_cfg(cross_attn_cfg, ATTENTION) for i in range(num_layers)]
         self.ffns = [build_from_cfg(ffn_cfg, FEEDFORWARD_NETWORK) for i in range(num_layers)]
         self.dim = self.self_attns[0].attn.qk_dim
+        self.return_all_layers = return_all_layers
         
         self.self_attns = nn.ModuleList(self.self_attns)
         self.cross_attns = nn.ModuleList(self.cross_attns)
         self.ffns = nn.ModuleList(self.ffns)
         self.shared_norm = build_norm_layer(shared_norm_cfg, self.dim)[1]
     
+    def init_weights(self):
+        # follow the official DETR to init parameters
+        for m in self.modules():
+            if hasattr(m, 'weight') and m.weight.dim() > 1:
+                xavier_init(m, distribution='uniform')
+        self._is_init = True
+
     def forward(self, embeds_pos, feats, feats_pos):
+        output = []
         embeds = torch.zeros_like(embeds_pos)
         for i in range(self.num_layers):
             embeds = self.self_attns[i](embeds, embeds_pos)
             embeds = self.cross_attns[i](embeds, feats, embeds_pos, feats_pos)
             embeds = self.ffns[i](embeds)
             embeds = self.shared_norm(embeds)
-        return embeds
+            output.append(embeds.unsqueeze(0))
+        output = torch.cat(output, dim=0)
+        if self.return_all_layers:
+            return output
+        else:
+            return output[-1]
+
+        
 
 @FEEDFORWARD_NETWORK.register_module()
 class DETREncoder(BaseModule):
@@ -57,6 +73,13 @@ class DETREncoder(BaseModule):
         self.self_attns = nn.ModuleList(self.self_attns)
         self.ffns = nn.ModuleList(self.ffns)
     
+    def init_weights(self):
+        # follow the official DETR to init parameters
+        for m in self.modules():
+            if hasattr(m, 'weight') and m.weight.dim() > 1:
+                xavier_init(m, distribution='uniform')
+        self._is_init = True
+
     def forward(self, feats, feats_pos):
         for i in range(self.num_layers):
             feats = self.self_attns[i](feats, feats_pos)
